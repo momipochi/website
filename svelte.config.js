8693aefd55855f96d8392fc1d00410626cff286b
unknown <k25650727@gmail.com>
2024-04-16 20:02:01 +0200

    commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,11 +9,8 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+			fallback: '200.html' // may differ from host to host
+		})
 	}
 };
 
